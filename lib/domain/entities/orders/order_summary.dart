abc97992418e0c4cb99d343c70b4bfdb7cb92241
kiadtisak9 <kiadtisak.j@extend-it-resource.com>
2025-02-18 19:42:24 +0700

    ✨ Add OrderSummary entity to represent order totals and discounts

---
+++ lib/domain/entities/orders/order_summary.dart
@@ -0,0 +1,11 @@
+class OrderSummary {
+  final double subtotal;
+  final double discount;
+  final double total;
+
+  OrderSummary({
+    required this.subtotal,
+    required this.discount,
+    required this.total,
+  });
+}
